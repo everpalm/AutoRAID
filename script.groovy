292484783fd76d4de172e35a14add9fc2c54ae63
Jaron Cheng <everpalm@gmail.com>
2024-06-08 15:37:54 +0800

    Chore: Invoke testmon with global scope

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildApp() {
 
 def testApp() {
     echo 'Start testing - 2'
-    sh "cd /home/pi/Projects/AutoRAID/tests && pipenv run python -m pytest test_w10_interface.py --testmon"
+    sh "cd /home/pi/Projects/AutoRAID/tests && pipenv run python -m pytest --testmon"
 }
 
 return this
