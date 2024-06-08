d914cd2619ab06f25df6a8621bc5919fc81e8357
Jaron Cheng <everpalm@gmail.com>
2024-06-08 15:32:54 +0800

    Chore: Fix invoking TEST_SUIT

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildApp() {
 
 def testApp() {
     echo 'Start testing - 2'
-    sh "cd /home/pi/Projects/AutoRAID/tests && pipenv run python -m pytest test_${params.MY_SUITE}.py --testmon"
+    sh "cd /home/pi/Projects/AutoRAID/tests && pipenv run python -m pytest test_w10_interface.py --testmon"
 }
 
 return this
