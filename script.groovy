cce87ccc6c041cd03f0e10153ec7866d2b0116a6
Jaron Cheng <everpalm@gmail.com>
2024-06-23 23:56:56 +0800

    Chore: Fix groovy testApp

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildApp() {
 
 def testApp() {
     echo 'Start testing - 2'
-    sh "cd /home/pi/Projects/AutoRAID/tests/test_amd_desktop && pipenv run python -m pytest tests/--testmon"
+    sh "cd /home/pi/Projects/AutoRAID/tests/test_amd_desktop && pipenv run python -m pytest --testmon"
 }
 
 return this
