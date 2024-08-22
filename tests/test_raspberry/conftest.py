fd93c033ff9f49faa32cd31118b76b8f0498902b
Jaron Cheng <everpalm@gmail.com>
2024-08-22 10:31:26 +0800

    Refactor: Move fixture of gpio to conftest.py

--- tests/test_raspberry/conftest.py
+++ tests/test_raspberry/conftest.py
@@ -1,5 +1,20 @@
 # Content of test_raspberry.conftest.py
 '''Copyright (c) 2024 Jaron Cheng'''
 import logging
+import pytest
+import RPi.GPIO as gpio
+
+from unit.gpio import OperateGPIO as og
 
 logging.getLogger('raspberry.pi3_gpio').setLevel(logging.DEBUG)
+
+@pytest.fixture(scope="module", autouse=True)
+def rpi_gpio(my_pins):
+    print('\n================== Setup Relay ==================')
+    amd_mgi = og(my_pins, gpio.BOARD)
+    
+    yield amd_mgi
+    print('\n================== Teardown Relay =====================')
+
+    # Clear GPIO
+    amd_mgi.clear_gpio()
--- tests/test_raspberry/test_pi3_gpio.py
+++ tests/test_raspberry/test_pi3_gpio.py
@@ -1,25 +1,25 @@
 # Contents of test_pi3_gpio.py
 import logging
-import pytest
-from unit.gpio import OperateGPIO as og
-import RPi.GPIO as gpio
+# import pytest
+# from unit.gpio import OperateGPIO as og
+# import RPi.GPIO as gpio
 
 logger = logging.getLogger(__name__)
 
 class TestPi3OperateGPIO(object):
-    @pytest.fixture(scope="module", autouse=True)
-    def amd_gpio(self, my_pins):
-        print('\n================== Setup Relay ==================')
-        amd_mgi = og(my_pins, gpio.BOARD)
+    # @pytest.fixture(scope="module", autouse=True)
+    # def amd_gpio(self, my_pins):
+    #     print('\n================== Setup Relay ==================')
+    #     amd_mgi = og(my_pins, gpio.BOARD)
         
-        yield amd_mgi
-        print('\n================== Teardown Relay =====================')
+    #     yield amd_mgi
+    #     print('\n================== Teardown Relay =====================')
 
-        # Clear GPIO
-        amd_mgi.clear_gpio()
+    #     # Clear GPIO
+    #     amd_mgi.clear_gpio()
 
-    def test_press_power_button(self, amd_gpio):
-        amd_gpio.press_power_button()
+    def test_press_power_button(self, rpi_gpio):
+        rpi_gpio.press_power_button()
         
         # Assert power state
 
