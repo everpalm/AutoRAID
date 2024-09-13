9e493d10962a0c83ce2a702d2be11c37df1b133c
Jaron Cheng <everpalm@gmail.com>
2024-09-13 15:37:15 +0800

    Refactor: Remove redundant snippets

--- tests/test_amd_desktop/conftest.py
+++ tests/test_amd_desktop/conftest.py
@@ -38,7 +38,6 @@ def my_mdb():
 def drone(drone_api):
     print('\n\033[32m================== Setup RSBPi =================\033[0m')
     return rpi(str_uart_path="/dev/ttyUSB0", int_baut_rate=115200,
-            #    str_file_name="/home/pi/Projects/AutoRAID/logs/uart.log",
                str_file_name="logs/uart.log", rpi_api=drone_api)
 
 @pytest.fixture(scope="module", autouse=True)
@@ -57,5 +56,4 @@ def target_perf(target_system):
 def my_app(cmdopt):
     print('\n\033[32m================== Setup API ===================\033[0m')
     return api(cmdopt.get('mode'), cmdopt.get('if_name'),
-        # cmdopt.get('config_file'), cmdopt.get('workspace'))
         cmdopt.get('config_file'))
