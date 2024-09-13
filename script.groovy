4df8d4d8ef0d37802a12cae603e7ae19f74c1867
Jaron Cheng <everpalm@gmail.com>
2024-09-13 08:48:22 +0800

    Refactor: Jenkinsfile

--- script.groovy
+++ script.groovy
@@ -1,10 +1,15 @@
-def buildApp() {
+def build_app() {
     echo 'Build - 1'
 }
 
-def testApp() {
-    echo 'Start testing - 2'
+def test_amd_desktop() {
+    echo 'test_amd_desktop'
     sh 'cd /home/pi/Projects/AutoRAID/tests/test_amd_desktop && pipenv run python -m pytest --testmon --private_token=$MY_PRIVATE_TOKEN'
+}
+
+def test_unit() {
+    echo 'test_unit'
+    sh 'cd /home/pi/Projects/AutoRAID/tests/test_unit && pipenv run python -m pytest --testmon --private_token=$MY_PRIVATE_TOKEN'
 }
 
 return this
