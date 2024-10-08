e075a58a828b386a5e4a9e2edcc29d85aa9cf437
Anny Tsai <everpalm@gmail.com>
2024-10-08 00:24:41 -0900

    Chore: Unit test amd64_event

--- tests/test_unit/test_amd64_event_unit.py
+++ tests/test_unit/test_amd64_event_unit.py
@@ -1,4 +1,20 @@
+from amd_desktop.amd64_event import WindowsEvent
 from collections import defaultdict
+from unittest.mock import MagicMock
+import pytest
+
+# 創建 platform_mock 的 fixture
+@pytest.fixture(scope="module")
+def platform_mock():
+    return MagicMock()
+
+
+# 創建 WindowsEvent 實例的 fixture，並注入 platform_mock
+@pytest.fixture(scope="function")
+def windows_event(platform_mock):
+    windows_event = WindowsEvent(platform_mock, 'config_file.json')
+    windows_event._api = MagicMock()  # 預設 Mock _api
+    return windows_event
 
 
 # 測試類別
