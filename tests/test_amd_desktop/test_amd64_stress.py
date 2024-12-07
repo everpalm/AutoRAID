0034aacf214102a6cd60bb8880d63d18bf33900c
Anny Tsai <everpalm@gmail.com>
2024-12-07 10:56:40 +0800

    Feat: Add oneshot of stress test

--- tests/test_amd_desktop/test_amd64_stress.py
+++ tests/test_amd_desktop/test_amd64_stress.py
@@ -18,6 +18,7 @@ FULL_READ = 0
 OLTP_LOADING = 30 # With 8 KB chunk size
 FULL_WRITE = 100
 OVER_NIGHT = 15
+ONE_SHOT = 15
 HYPER_THREAD = 2
 SINGLE_THREAD = 1
 MIN_IODEPTH = 1
@@ -51,11 +52,23 @@ def test_check_error(win_event):
 
     yield win_event.clear_error()
 
+    # if win_event.find_error("System", 51, r'An error was detected on device (\\\w+\\\w+\.+)'):
+    #     raise AssertionError("Error 51 detected in system logs.")
+    
+    # if win_event.find_error("System", 157, r'Disk (\d+) has been surprise removed.'):
+    #     raise AssertionError("Error 157 detected in system logs.")
+    errors = []
     if win_event.find_error("System", 51, r'An error was detected on device (\\\w+\\\w+\.+)'):
-        raise AssertionError("Error 51 detected in system logs.")
+        # raise AssertionError("Error 51 detected in system logs.")
+        errors.append("Error 51 detected in system logs.")
     
     if win_event.find_error("System", 157, r'Disk (\d+) has been surprise removed.'):
-        raise AssertionError("Error 157 detected in system logs.")
+        # raise AssertionError("Error 157 detected in system logs.")
+        errors.append("Error 157 detected: Disk surprise removal.")
+    
+    if errors:
+        logger.error(f"Windows event errors detected: {errors}")
+        raise AssertionError(f"Detected errors: {errors}")  
     
     print('\n\033[32m================== Teardown Win Event ==========\033[0m')
 
@@ -105,6 +118,29 @@ class TestAMD64MultiPathStress:
         log_io_metrics(read_bw, read_iops, write_bw, write_iops, 'stress_')
 
         criteria = my_mdb.aggregate_stress_metrics(write_pattern, iodepth)
+
+        logger.debug(f'criteria = {criteria}')
+        
+    def test_oneshot_operation(self, target_stress,my_mdb):
+        """Runs oneshot I/O operations to test system stress with optimum
+        I/O depths and write patterns.
+        
+        Args:
+            target_stress (AMD64MultiPathStress): Stress instance for I/O tests.
+            write_pattern (int): Write pattern defining full read.
+            iodepth (int): Optimum I/O depth level for stress testing is 7.
+            my_mdb: Mock database for storing and comparing test metrics.
+        
+        Assertions:
+            - read_bw, read_iops, write_bw, write_iops metrics meet target criteria.
+        """
+        read_bw, read_iops, write_bw, write_iops = \
+        target_stress.run_io_operation(SINGLE_THREAD, OPTIMUM_IODEPTH, '4k',
+                                       '4k', FULL_READ, ONE_SHOT)
+    
+        log_io_metrics(read_bw, read_iops, write_bw, write_iops, 'stress_')
+
+        criteria = my_mdb.aggregate_stress_metrics(FULL_READ, OPTIMUM_IODEPTH)
 
         logger.debug(f'criteria = {criteria}')
         
