ba0a546900db31a7e41e456782f02a517f7a71a1
Anny Tsai <everpalm@gmail.com>
2024-12-09 13:45:26 +0800

    Feat: One shot stress test suite

--- tests/test_amd_desktop/test_amd64_stress.py
+++ tests/test_amd_desktop/test_amd64_stress.py
@@ -18,7 +18,8 @@ FULL_READ = 0
 OLTP_LOADING = 30 # With 8 KB chunk size
 FULL_WRITE = 100
 OVER_NIGHT = 15
-ONE_SHOT = 15
+HALF_RW = 50
+ONE_SHOT = 30
 HYPER_THREAD = 2
 SINGLE_THREAD = 1
 MIN_IODEPTH = 1
@@ -52,18 +53,11 @@ def test_check_error(win_event):
 
     yield win_event.clear_error()
 
-    # if win_event.find_error("System", 51, r'An error was detected on device (\\\w+\\\w+\.+)'):
-    #     raise AssertionError("Error 51 detected in system logs.")
-    
-    # if win_event.find_error("System", 157, r'Disk (\d+) has been surprise removed.'):
-    #     raise AssertionError("Error 157 detected in system logs.")
     errors = []
     if win_event.find_error("System", 51, r'An error was detected on device (\\\w+\\\w+\.+)'):
-        # raise AssertionError("Error 51 detected in system logs.")
         errors.append("Error 51 detected in system logs.")
     
     if win_event.find_error("System", 157, r'Disk (\d+) has been surprise removed.'):
-        # raise AssertionError("Error 157 detected in system logs.")
         errors.append("Error 157 detected: Disk surprise removal.")
     
     if errors:
@@ -82,19 +76,6 @@ class TestAMD64MultiPathStress:
             sdid: The Sub-device ID of PCIe, confirm SDID of PCI device in
             advance
     '''
-    # @pytest.fixture(scope="function")
-    # def target_stress(self, target_system):
-    #     """Fixture for setting up an AMD64MultiPathStress instance for I/O stress tests.
-        
-    #     Args:
-    #         target_system: The system instance to run stress tests on.
-        
-    #     Returns:
-    #         AMD64MultiPathStress: Instance for executing stress test operations.
-    #     """
-    #     print('\n\033[32m================ Setup I/O Stress ==========\033[0m')
-    #     return amps(platform=target_system)
-
     # @pytest.mark.parametrize('iodepth', list(range(MIN_IODEPTH, MAX_IODEPTH)))
     @pytest.mark.parametrize('iodepth', [2**power for power in range(6)])
     @pytest.mark.parametrize('write_pattern', [FULL_READ, FULL_WRITE])
@@ -120,4 +101,74 @@ class TestAMD64MultiPathStress:
         criteria = my_mdb.aggregate_stress_metrics(write_pattern, iodepth)
 
         logger.debug(f'criteria = {criteria}')
+
+class TestOneShotStress:
+    def test_read_write(self, target_stress,my_mdb):
+            """Runs oneshot I/O operations to test system stress with optimum
+            I/O depths and write patterns.
+            
+            Args:
+                target_stress (AMD64MultiPathStress): Stress instance for I/O tests.
+                write_pattern (int): Write and Read in half.
+                iodepth (int): Optimum I/O depth level for stress testing is 7.
+                my_mdb: Mock database for storing and comparing test metrics.
+            
+            Assertions:
+                - read_bw, read_iops, write_bw, write_iops metrics meet target criteria.
+            """
+            read_bw, read_iops, write_bw, write_iops = \
+            target_stress.run_io_operation(SINGLE_THREAD, OPTIMUM_IODEPTH, '4k',
+                                        '4k', HALF_RW, ONE_SHOT)
+        
+            log_io_metrics(read_bw, read_iops, write_bw, write_iops, 'stress_')
+
+            criteria = my_mdb.aggregate_stress_metrics(HALF_RW, OPTIMUM_IODEPTH)
+
+            logger.debug(f'criteria = {criteria}')
+
+    def test_full_read(self, target_stress,my_mdb):
+            """Runs oneshot I/O operations to test system stress with optimum
+            I/O depths and write patterns.
+            
+            Args:
+                target_stress (AMD64MultiPathStress): Stress instance for I/O tests.
+                write_pattern (int): Write pattern defining full read.
+                iodepth (int): Optimum I/O depth level for stress testing is 7.
+                my_mdb: Mock database for storing and comparing test metrics.
+            
+            Assertions:
+                - read_bw, read_iops, write_bw, write_iops metrics meet target criteria.
+            """
+            read_bw, read_iops, write_bw, write_iops = \
+            target_stress.run_io_operation(SINGLE_THREAD, OPTIMUM_IODEPTH, '4k',
+                                        '4k', FULL_READ, ONE_SHOT)
+        
+            log_io_metrics(read_bw, read_iops, write_bw, write_iops, 'stress_')
+
+            criteria = my_mdb.aggregate_stress_metrics(FULL_READ, OPTIMUM_IODEPTH)
+
+            logger.debug(f'criteria = {criteria}')
+
+    def test_full_write(self, target_stress,my_mdb):
+            """Runs oneshot I/O operations to test system stress with optimum
+            I/O depths and write patterns.
+            
+            Args:
+                target_stress (AMD64MultiPathStress): Stress instance for I/O tests.
+                write_pattern (int): Write pattern defining full write.
+                iodepth (int): Optimum I/O depth level for stress testing is 7.
+                my_mdb: Mock database for storing and comparing test metrics.
+            
+            Assertions:
+                - read_bw, read_iops, write_bw, write_iops metrics meet target criteria.
+            """
+            read_bw, read_iops, write_bw, write_iops = \
+            target_stress.run_io_operation(SINGLE_THREAD, OPTIMUM_IODEPTH, '4k',
+                                        '4k', FULL_WRITE, ONE_SHOT)
+        
+            log_io_metrics(read_bw, read_iops, write_bw, write_iops, 'stress_')
+
+            criteria = my_mdb.aggregate_stress_metrics(FULL_WRITE, OPTIMUM_IODEPTH)
+
+            logger.debug(f'criteria = {criteria}')
         
