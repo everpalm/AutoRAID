ba2fc773f2c55517fa933105060f01263b57c357
Anny Tsai <everpalm@gmail.com>
2024-12-10 13:04:27 +0800

    Chore: Tirgger another pipeline after success

--- script.groovy
+++ script.groovy
@@ -7,33 +7,41 @@ def test_amd_desktop() {
     sh 'pipenv run pytest ${TEST_AMD_DESKTOP} --testmon --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
 }
 
-def test_unit() {
-    echo 'test_unit'
-    sh 'pipenv run pytest ${TEST_UNIT} --testmon --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
-}
-
 def test_raspberry() {
     echo 'test_raspberry'
-    sh 'pipenv run pytest --testmon --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
+    sh 'pipenv run pytest --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
 }
 
 def test_amd64_nvme() {
-    sh 'pipenv run pytest ${TEST_AMD_DESKTOP}/test_amd64_nvme.py --testmon --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
+    sh 'pipenv run pytest ${TEST_AMD_DESKTOP}/test_amd64_nvme.py --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
 }
 
 def test_amd64_perf() {
-    sh 'pipenv run pytest ${TEST_AMD_DESKTOP}/test_amd64_perf.py --testmon --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
+    sh 'pipenv run pytest ${TEST_AMD_DESKTOP}/test_amd64_perf.py --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
 }
 
 def test_amd64_stress() {
-    sh 'pipenv run pytest ${TEST_AMD_DESKTOP}/test_amd64_stress.py --testmon --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
+    sh 'pipenv run pytest ${TEST_AMD_DESKTOP}/test_amd64_stress.py --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
 }
 def test_application_interface() {
-    sh 'pipenv run pytest ${TEST_AMD_DESKTOP}/test_application_interface.py --testmon --private_token=$MY_PRIVATE_TOKEN'
+    sh 'pipenv run pytest ${TEST_AMD_DESKTOP}/test_application_interface.py --private_token=$MY_PRIVATE_TOKEN'
 }
 
 def test_pep8() {
     // sh 'pipenv run pylint ${TEST_UNIT} --exit-zero' //Forcibly pass
     sh 'pipenv run pylint ${TEST_UNIT} --fail-under=7.0'
+}
+
+def test_smoke() {
+    sh 'pipenv run pytest ${TEST_AMD_DESKTOP}/test_amd64_warmboot.py --testmon --private_token=$MY_PRIVATE_TOKEN --cov-report=html --count=3 --repeat-scope=module'    
+}
+
+def test_sanity() {
+    sh 'pipenv run pytest ${TEST_AMD_DESKTOP} --testmon --cov-report=html'    
+}
+
+def test_unit() {
+    echo 'test_unit'
+    sh 'pipenv run pytest ${TEST_UNIT} --private_token=$MY_PRIVATE_TOKEN --cov-report=html'
 }
 return this
