d513eb0e39b7621998f487b24ae77ceb1822ef29
Anny Tsai <everpalm@gmail.com>
2024-12-14 13:21:15 +0800

    Perf: Expect max performance after 156 seconds

--- tests/test_amd_desktop/test_amd64_stress.py
+++ tests/test_amd_desktop/test_amd64_stress.py
@@ -17,54 +17,15 @@ logger = logging.getLogger(__name__)
 FULL_READ = 0
 OLTP_LOADING = 30 # With 8 KB chunk size
 FULL_WRITE = 100
-OVER_NIGHT = 15
+OVER_NIGHT = 156
 HALF_RW = 50
-ONE_SHOT = 30
+ONE_SHOT = 156
 HYPER_THREAD = 2
 SINGLE_THREAD = 1
 MIN_IODEPTH = 1
 MAX_IODEPTH = 33
 OPTIMUM_IODEPTH = 7
 
-# @pytest.fixture(scope="function")
-# def win_event(target_system):
-#     """Fixture for setting up Windows Event monitoring for system errors.
-
-#     Args:
-#         target_system: The system instance to monitor for Windows Event logs.
-
-#     Returns:
-#         WindowsEvent: An instance of WindowsEvent for error logging.
-#     """
-#     print('\n\033[32m================== Setup Win Event =============\033[0m')
-#     return we(platform=target_system)
-
-# @pytest.fixture(scope="function", autouse=True)
-# def test_check_error(win_event):
-#     """Fixture to clear previous Windows event logs and check for specific errors
-#     after each test function.
-
-#     Yields:
-#         Clears event logs and checks for errors upon test completion.
-
-#     Raises:
-#         AssertionError: If specific errors (ID 51 or 157) are detected in logs.
-#     """
-
-#     yield win_event.clear_error()
-
-#     errors = []
-#     if win_event.find_error("System", 51, r'An error was detected on device (\\\w+\\\w+\.+)'):
-#         errors.append("Error 51 detected in system logs.")
-
-#     if win_event.find_error("System", 157, r'Disk (\d+) has been surprise removed.'):
-#         errors.append("Error 157 detected: Disk surprise removal.")
-
-#     if errors:
-#         logger.error("Windows event errors detected: %s", errors)
-#         raise AssertionError(f"Detected errors: {errors}")
-
-#     print('\n\033[32m================== Teardown Win Event ==========\033[0m')
 
 class TestAMD64MultiPathStress:
     ''' Test I/O Stress 
