37eae1fd86f06f276293895b1297710f807e3c5d
Anny Tsai <everpalm@gmail.com>
2024-12-30 15:10:39 +0800

    Style: Use "system" to represent a platform name

--- tests/test_amd_desktop/test_amd64_os.py
+++ tests/test_amd_desktop/test_amd64_os.py
@@ -8,124 +8,124 @@ import pytest
 logger = logging.getLogger(__name__)
 
 with open('config/amd64_nvme.json', 'r', encoding='utf-8') as f:
-    AMD64_NVM = [json.load(f)]
+    AMD64_SETTINGS = [json.load(f)]
 
 
-class TestAMD64Windows:
+class TestAMD64System:
     '''Duplicate of TestAMD64NVMe'''
-    @pytest.mark.parametrize('amd64_nvm', AMD64_NVM)
-    def test_get_hyperthreading(self, amd64_windows, amd64_nvm):
+    @pytest.mark.parametrize('AMD64_SETTINGS', AMD64_SETTINGS)
+    def test_get_hyperthreading(self, amd64_system, AMD64_SETTINGS):
         """Test for verifying hyperthreading setting.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
-        logger.info("hyperthreading = %s", amd64_windows.hyperthreading)
-        assert (amd64_windows.hyperthreading ==
-                amd64_nvm['CPU Information']['Hyperthreading'])
+        logger.info("hyperthreading = %s", amd64_system.hyperthreading)
+        assert (amd64_system.hyperthreading ==
+                AMD64_SETTINGS['CPU Information']['Hyperthreading'])
 
-    def test_memory_size(self, amd64_windows):
+    def test_memory_size(self, amd64_system):
         """Test for verifying memory size of the system.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
-        memory_size = amd64_windows.memory_size
+        memory_size = amd64_system.memory_size
         logger.info("Memory Size = %s GB", memory_size)
 
-    # @pytest.mark.parametrize('amd64_nvm', AMD64_NVM)
-    def test_mac_address(self, amd64_windows):
+    # @pytest.mark.parametrize('AMD64_SETTINGS', AMD64_SETTINGS)
+    def test_mac_address(self, amd64_system):
         """Test for verifying MAC address of the system.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
-        mac_address = amd64_windows.mac_address
+        mac_address = amd64_system.mac_address
         logger.info("MAC Address = %s", mac_address)
 
-    @pytest.mark.parametrize('amd64_nvm', AMD64_NVM)
-    def test_get_cpu_info(self, amd64_windows, amd64_nvm):
+    @pytest.mark.parametrize('AMD64_SETTINGS', AMD64_SETTINGS)
+    def test_get_cpu_info(self, amd64_system, AMD64_SETTINGS):
         """Test for verifying CPU model and core count.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
-        logger.info('CPU(s) = %s, CPU model = %s', amd64_windows.cpu_num,
-                    amd64_windows.cpu_name)
-        assert (amd64_windows.cpu_num ==
-                amd64_nvm['CPU Information']["CPU(s)"])
-        assert (amd64_windows.cpu_name ==
-                amd64_nvm['CPU Information']["Model Name"])
+        logger.info('CPU(s) = %s, CPU model = %s', amd64_system.cpu_num,
+                    amd64_system.cpu_name)
+        assert (amd64_system.cpu_num ==
+                AMD64_SETTINGS['CPU Information']["CPU(s)"])
+        assert (amd64_system.cpu_name ==
+                AMD64_SETTINGS['CPU Information']["Model Name"])
 
-    @pytest.mark.parametrize('amd64_nvm', AMD64_NVM)
-    def test_get_desktop_info(self, amd64_windows, amd64_nvm):
+    @pytest.mark.parametrize('AMD64_SETTINGS', AMD64_SETTINGS)
+    def test_get_desktop_info(self, amd64_system, AMD64_SETTINGS):
         """Test for verifying system manufacturer, model, and operating system.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
         logger.info('Manufacturer = %s, Model = %s, Name = %s',
-                    amd64_windows.vendor,
-                    amd64_windows.model,
-                    amd64_windows.name)
-        assert (amd64_windows.vendor ==
-                amd64_nvm['Desktop Information']["Manufacturer"])
-        assert (amd64_windows.model ==
-                amd64_nvm['Desktop Information']["Model"])
+                    amd64_system.vendor,
+                    amd64_system.model,
+                    amd64_system.name)
+        assert (amd64_system.vendor ==
+                AMD64_SETTINGS['Desktop Information']["Manufacturer"])
+        assert (amd64_system.model ==
+                AMD64_SETTINGS['Desktop Information']["Model"])
 
-    @pytest.mark.parametrize('amd64_nvm', AMD64_NVM)
-    def test_get_pcie_info(self, amd64_windows, amd64_nvm):
+    @pytest.mark.parametrize('AMD64_SETTINGS', AMD64_SETTINGS)
+    def test_get_pcie_info(self, amd64_system, AMD64_SETTINGS):
         """Test for verifying PCIe configuration, including VID, DID, SDID,
         and Rev.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
-        logger.info('VID = %s', amd64_windows.vid)
-        logger.info('DID = %s', amd64_windows.did)
-        logger.info('SDID = %s', amd64_windows.sdid)
-        logger.info('Rev = %s', amd64_windows.rev)
+        logger.info('VID = %s', amd64_system.vid)
+        logger.info('DID = %s', amd64_system.did)
+        logger.info('SDID = %s', amd64_system.sdid)
+        logger.info('Rev = %s', amd64_system.rev)
 
-        assert (amd64_windows.vid ==
-                amd64_nvm['PCIE Configuration']["VID"])
-        assert (amd64_windows.did ==
-                amd64_nvm['PCIE Configuration']["DID"])
-        assert (amd64_windows.sdid ==
-                amd64_nvm['PCIE Configuration']["SDID"])
-        assert (amd64_windows.rev ==
-                amd64_nvm['PCIE Configuration']["Rev"])
+        assert (amd64_system.vid ==
+                AMD64_SETTINGS['PCIE Configuration']["VID"])
+        assert (amd64_system.did ==
+                AMD64_SETTINGS['PCIE Configuration']["DID"])
+        assert (amd64_system.sdid ==
+                AMD64_SETTINGS['PCIE Configuration']["SDID"])
+        assert (amd64_system.rev ==
+                AMD64_SETTINGS['PCIE Configuration']["Rev"])
 
-    @pytest.mark.parametrize('amd64_nvm', AMD64_NVM)
-    def test_get_disk_num(self, amd64_windows, amd64_nvm):
+    @pytest.mark.parametrize('AMD64_SETTINGS', AMD64_SETTINGS)
+    def test_get_disk_num(self, amd64_system, AMD64_SETTINGS):
         """Test for verifying the number of disks and the serial number.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
-        logger.info("Number = %s", amd64_windows.disk_num)
-        logger.info("SerialNumber = %s", amd64_windows.serial_num)
-        assert (amd64_windows.disk_num ==
-                amd64_nvm['Disk Information']["Number"])
+        logger.info("Number = %s", amd64_system.disk_num)
+        logger.info("SerialNumber = %s", amd64_system.serial_num)
+        assert (amd64_system.disk_num ==
+                AMD64_SETTINGS['Disk Information']["Number"])
 
-    @pytest.mark.parametrize('amd64_nvm', AMD64_NVM)
-    def test_get_volume(self, amd64_windows, amd64_nvm):
+    @pytest.mark.parametrize('AMD64_SETTINGS', AMD64_SETTINGS)
+    def test_get_volume(self, amd64_system, AMD64_SETTINGS):
         """Test for verifying disk volume information.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
         for i in range(12):  # 迴圈處理 0 到 11
             try:
-                logger.info('%s = %s', amd64_windows.disk_info[i][0],
-                            amd64_windows.disk_info[i][1])
+                logger.info('%s = %s', amd64_system.disk_info[i][0],
+                            amd64_system.disk_info[i][1])
             except IndexError:
                 break  # 如果索引超出範圍，則跳出迴圈
 
@@ -133,27 +133,27 @@ class TestAMD64Windows:
                         "P"]
         for i, letter in enumerate(disk_letters):
             try:
-                assert amd64_windows.disk_info[i][1] == \
-                    amd64_nvm['Disk Information'][letter]
+                assert amd64_system.disk_info[i][1] == \
+                    AMD64_SETTINGS['Disk Information'][letter]
             except (IndexError, KeyError):  # 處理索引或鍵錯誤
                 pass  # 或是選擇其他處理方式，例如記錄錯誤訊息
 
-    def test_partition_size(self, amd64_windows):
+    def test_partition_size(self, amd64_system):
         """Test for verifying parition size of the disk.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
-        partition_size = amd64_windows.partition_size
+        partition_size = amd64_system.partition_size
         logger.info("Partition Size = %s GB", partition_size)
 
-    def test_disk_capacity(self, amd64_windows):
+    def test_disk_capacity(self, amd64_system):
         """Test for verifying parition size of the disk.
 
         Args:
-            amd64_windows: The system instance being tested.
-            amd64_nvm (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            AMD64_SETTINGS (dict): Expected configuration data for validation.
         """
-        disk_capacity = amd64_windows.disk_capacity
+        disk_capacity = amd64_system.disk_capacity
         logger.info("disk_capacity = %s", disk_capacity)
