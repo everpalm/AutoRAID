938c82e4eadbbad8017bb68481d2ab3c2116c7d2
Anny Tsai <everpalm@gmail.com>
2025-01-02 17:55:54 +0800

    Feat: Commandline test (Adapter)

---
+++ tests/test_storage_device/test_commandline.py
@@ -0,0 +1,35 @@
+# Contents of test_commandline.py
+'''Unit tests for the ApplicationInterface class, which includes testing the
+   execution of Windows commands to verify system responses and configurations
+
+   Copyright (c) 2024 Jaron Cheng
+'''
+import json
+import logging
+import pytest
+
+from unit.commandline_interface import CLIFactory
+
+# Set up logger
+logger = logging.getLogger(__name__)
+
+with open('config/test_commandline.json', 'r', encoding='utf-8') as f:
+    TEST_PLAN = json.load(f)
+
+
+@pytest.fixture(scope="module")
+def mnv_cli(network_api, amd64_system):
+    console = CLIFactory(network_api)
+    print('\n\033[32m================== Setup Command Test ===========\033[0m')
+    yield console.initiate(platform=amd64_system)
+    print('\n\033[32m================== Teardown Command Test=========\033[0m')
+
+
+class TestCLI:
+    '''docstring'''
+    @pytest.mark.parametrize('test_plan', TEST_PLAN)
+    def test_list_adapter(self, mnv_cli, test_plan):
+        '''docstring'''
+        result = mnv_cli.interpret(test_plan["Test Case"])
+        logger.debug('result = %s', result)
+        assert result == test_plan["Expected"]
---
+++ unit/commandline_interface.py
@@ -0,0 +1,72 @@
+# Contents of commandline_interface.py
+'''Copyright (c) 2024 Jaron Cheng'''
+import logging
+from abc import ABC
+from abc import abstractmethod
+from amd_desktop.amd64_system import BaseOS
+from unit.log_handler import get_logger
+
+logger = get_logger(__name__, logging.DEBUG)
+
+
+class BaseCLI(ABC):
+    '''This is a docstring'''
+    def __init__(self, platform: BaseOS):
+        '''This is a docstring'''
+        self.api = platform.api
+
+    @abstractmethod
+    def interpret(self) -> str:
+        '''Placeholder'''
+
+
+class WindowsCLI(BaseCLI):
+    '''This is a docstring'''
+    PREFIX = 'mnv_cli.exe'
+
+    def interpret(self, command: str):
+        '''placeholder'''
+
+        try:
+            composed_cmd = f'{self.PREFIX} {command}'
+            logger.debug('composed_cmd = %s', composed_cmd)
+            cmd_output = self.api.command_line.original(
+                self.api,
+                composed_cmd
+            )
+        except Exception as e:
+            logger.error('Error occurred in interpretation: %s', e)
+            raise
+        return cmd_output
+
+
+class LinuxCLI(WindowsCLI):
+    '''This is a docstring'''
+    PREFIX = 'mnv_cli'
+
+    def interpret(self, command: str):
+        pass
+
+
+class BaseCLIFactory(ABC):
+    def __init__(self, api: BaseCLI):
+        '''docstring'''
+        self.api = api
+        self.os_type = api.os_type
+
+    @abstractmethod
+    def initiate(self, **kwargs) -> BaseCLI:
+        '''docstring'''
+        pass
+
+
+class CLIFactory(BaseCLIFactory):
+    '''docstring'''
+    def initiate(self, **kwargs) -> BaseCLI:
+        '''docstring'''
+        if self.os_type == 'Windows':
+            return WindowsCLI(**kwargs)
+        elif self.os_type == 'Linux':
+            return LinuxCLI(**kwargs)
+        else:
+            raise ValueError(f"Unsupported OS type: {self.os_type}")
