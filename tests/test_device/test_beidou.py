8ff48fa3ffd7887a4f3366d90292e68818a6a680
everpalm <everpalm@ms58.url.com.tw>
2025-02-04 16:31:04 +0800

    Chore: Debug logger

--- tests/test_device/test_beidou.py
+++ tests/test_device/test_beidou.py
@@ -14,31 +14,31 @@ class TestChunghua:
         '''fixture'''
         controller_info = boot_device.controller_info
         mapping_table = network_api.nvme_controller
-        logger.info("bus_device_func = %s", controller_info.bus_device_func)
-        logger.info("device = %s", controller_info.device)
-        logger.info("slot_id = %s", controller_info.slot_id)
-        logger.info("vid = %s", controller_info.vid)
-        logger.info("svid = %s", controller_info.svid)
-        logger.info("did = %s", controller_info.did)
-        logger.info("sdid = %s", controller_info.sdid)
-        logger.info("revision_id = %s", controller_info.revision_id)
-        logger.info("port_count = %s", controller_info.port_count)
-        logger.info("max_pd_of_per_vd = %s", controller_info.max_pd_of_per_vd)
-        logger.info("max_vd = %s", controller_info.max_vd)
-        logger.info("max_pd = %s", controller_info.max_pd)
-        logger.info("max_ns_of_per_vd = %s", controller_info.max_ns_of_per_vd)
-        logger.info("max_ns = %s", controller_info.max_ns)
-        logger.info("supported_raid_mode = %s",
-                    controller_info.supported_raid_mode)
-        logger.info("cache = %s", controller_info.cache)
-        logger.info("supported_bga_features = %s",
-                    controller_info.supported_bga_features)
-        logger.info("support_stripe_size = %s",
-                    controller_info.support_stripe_size)
-        logger.info("supported_features = %s",
-                    controller_info.supported_features)
-        logger.info("root_complexes = %s", controller_info.root_complexes)
-        logger.info("end_points = %s", controller_info.end_points)
+        logger.debug("bus_device_func = %s", controller_info.bus_device_func)
+        logger.debug("device = %s", controller_info.device)
+        logger.debug("slot_id = %s", controller_info.slot_id)
+        logger.debug("vid = %s", controller_info.vid)
+        logger.debug("svid = %s", controller_info.svid)
+        logger.debug("did = %s", controller_info.did)
+        logger.debug("sdid = %s", controller_info.sdid)
+        logger.debug("revision_id = %s", controller_info.revision_id)
+        logger.debug("port_count = %s", controller_info.port_count)
+        logger.debug("max_pd_of_per_vd = %s", controller_info.max_pd_of_per_vd)
+        logger.debug("max_vd = %s", controller_info.max_vd)
+        logger.debug("max_pd = %s", controller_info.max_pd)
+        logger.debug("max_ns_of_per_vd = %s", controller_info.max_ns_of_per_vd)
+        logger.debug("max_ns = %s", controller_info.max_ns)
+        logger.debug("supported_raid_mode = %s",
+                     controller_info.supported_raid_mode)
+        logger.debug("cache = %s", controller_info.cache)
+        logger.debug("supported_bga_features = %s",
+                     controller_info.supported_bga_features)
+        logger.debug("support_stripe_size = %s",
+                     controller_info.support_stripe_size)
+        logger.debug("supported_features = %s",
+                     controller_info.supported_features)
+        logger.debug("root_complexes = %s", controller_info.root_complexes)
+        logger.debug("end_points = %s", controller_info.end_points)
 
         assert controller_info.bus_device_func == mapping_table.bus_device_func
         assert controller_info.device == mapping_table.device
