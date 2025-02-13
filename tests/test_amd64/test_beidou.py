80cb8a4003966db3ddb2fe2e2c83d4319569ecc7
everpalm <everpalm@ms58.url.com.tw>
2025-02-13 13:11:36 +0800

    Feat: Add CPU property test

--- tests/test_amd64/test_beidou.py
+++ tests/test_amd64/test_beidou.py
@@ -1,71 +1,63 @@
-# Contents of tests/test_amd6/test_beidou.py
+# Contents of tests/test_amd64/test_beidou.py
 '''Copyright (c) 2025 Jaron Cheng'''
 import logging
-import pytest
-from system.amd64 import PlatformFactory
+# import pytest
+# from system.amd64 import PlatformFactory
 
 # Set up logger
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope="module")
-def beidou(network_api):
-    """
-    docstring
-    """
-    print("\n\033[32m================== Setup Beidou ================\033[0m")
-    factory = PlatformFactory(network_api)
-    return factory.create_platform(interface=network_api)
-
-
 class TestBeidou:
     '''Duplicate of Beidou configuration'''
-    def test_get_cpu_info(self, beidou):
+    def test_get_cpu_info(self, amd64_system):
         """Test for verifying hyperthreading setting.
 
         Args:
             beidou: The system instance being tested.
         """
-        logger.info("CPU(s) = %s", beidou.cpu_num)
-        logger.info("CPU model = %s", beidou.cpu_name)
-        logger.info("hyperthreading = %s", beidou.hyperthreading)
-        assert (beidou.hyperthreading ==
-                beidou.api.cpu.hyperthreading)
-        assert beidou.cpu_num == beidou.api.cpu.cores
-        assert beidou.cpu_name == beidou.api.cpu.model_name
+        logger.info("CPU(s) = %s", amd64_system.cpu_num)
+        logger.info("CPU model = %s", amd64_system.cpu_name)
+        logger.info("hyperthreading = %s", amd64_system.hyperthreading)
+        assert (amd64_system.hyperthreading ==
+                amd64_system.api.cpu.hyperthreading)
+        assert amd64_system.cpu_num == amd64_system.api.cpu.cores
+        assert amd64_system.cpu_name == amd64_system.api.cpu.model_name
 
-    def test_get_system_info(self, beidou):
+    def test_get_system_info(self, amd64_system):
         """Test for verifying system manufacturer, model, and operating system.
 
         Args:
-            beidou: The system instance being tested.
-            beidou (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            amd64_system (dict): Expected configuration data for validation.
         """
-        logger.info('Manufacturer = %s', beidou.vendor)
-        logger.info('Model = %s', beidou.model)
-        logger.info('Name = %s', beidou.name)
-        logger.info('Total Memory Size = %s GB', beidou.memory_size)
-        assert beidou.vendor == beidou.api.system.manufacturer
-        assert beidou.model == beidou.api.system.model
-        assert beidou.name == beidou.api.system.name
-        assert str(beidou.memory_size) + ' GB' == beidou.api.system.memory
+        logger.info('Manufacturer = %s', amd64_system.vendor)
+        logger.info('Model = %s', amd64_system.model)
+        logger.info('Name = %s', amd64_system.name)
+        logger.info('Total Memory Size = %s GB', amd64_system.memory_size)
+        assert amd64_system.vendor == amd64_system.api.system.manufacturer
+        assert amd64_system.model == amd64_system.api.system.model
+        assert amd64_system.name == amd64_system.api.system.name
+        assert (str(amd64_system.memory_size) + ' GB' ==
+                amd64_system.api.system.memory)
 
-    # def test_memory_size(self, beidou):
-    #     """Test for verifying memory size of the system.
-
-    #     Args:
-    #         beidou: The system instance being tested.
-    #         beidou (dict): Expected configuration data for validation.
-    #     """
-    #     memory_size = beidou.memory_size
-    #     logger.info("Memory Size = %s GB", memory_size)
-
-    def test_mac_address(self, beidou):
+    def test_mac_address(self, amd64_system):
         """Test for verifying MAC address of the system.
 
         Args:
-            beidou: The system instance being tested.
-            beidou (dict): Expected configuration data for validation.
+            amd64_system: The system instance being tested.
+            amd64_system (dict): Expected configuration data for validation.
         """
-        mac_address = beidou.mac_address
-        logger.info("MAC Address = %s", mac_address)
+        logger.info("MAC Address = %s", amd64_system.mac_address)
+        assert amd64_system.mac_address == amd64_system.api.network.mac_address
+
+    def test_cpu(self, amd64_system):
+        '''docstring'''
+        logger.info("Manufacturer = %s", amd64_system.cpu.vendor)
+        logger.info("CPU(s) = %s", amd64_system.cpu.cores)
+        logger.info("CPU model = %s", amd64_system.cpu.model)
+        logger.info("hyperthreading = %s", amd64_system.cpu.hyperthreading)
+        assert (amd64_system.cpu.hyperthreading ==
+                amd64_system.api.cpu.hyperthreading)
+        assert amd64_system.cpu.cores == amd64_system.api.cpu.cores
+        assert amd64_system.cpu.model == amd64_system.api.cpu.model_name
